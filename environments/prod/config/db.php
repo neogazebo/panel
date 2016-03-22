f53d6089c1128ebb0198d698b510a307339d3498
ilham <ilham@ebizu.com>
2016-03-22 10:45:07 +0700

    initial environments and install.sh

---
+++ environments/prod/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db.ebizu.com;dbname=ebdb',
+    'username' => 'ebizu',
+    'password' => 'ebizu4ebizu',
+    'charset' => 'utf8',
+];
---
+++ environments/staging/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=db.ebizu.com;dbname=ebdb-staging',
+    'username' => 'ebizu',
+    'password' => 'ebizu4ebizu',
+    'charset' => 'utf8',
+];
