c0ab5048bacd12fd493d3687445b0d8dc1ec67b1
Elvis <elvis@ebizu.com>
2016-04-07 18:51:05 +0700

    -

--- themes/AdminLTE/layouts/main.php
+++ themes/AdminLTE/layouts/main.php
@@ -28,7 +28,7 @@ $this->registerCss("
     </head>
 
     <body class="hold-transition skin-blue sidebar-mini">
-        <?= var_dump(Yii::$app->user); exit; ?>
+        <?= var_dump(Yii::$app->user->identity); exit; ?>
         <?php $this->beginBody(); ?>
         <div class="wrapper">
             <header class="main-header">
