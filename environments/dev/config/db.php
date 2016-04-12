46742a68b030f262df5d470fb415453fc6b045a0
Elvis <elvis@ebizu.com>
2016-04-12 14:54:45 +0700

    reconfiguration of db dev env

--- environments/dev/config/db.php
+++ environments/dev/config/db.php
@@ -3,7 +3,7 @@
 return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=ebizu_db',
-    'username' => 'root',
-    'password' => 'root',
+    'username' => 'ebizu',
+    'password' => 'ebizu4ebizu',
     'charset' => 'utf8',
 ];
