d08c62f1636be687645f0ad2c6bcc10ee6fdd830
Elvis <elvis@ebizu.com>
2016-04-12 16:01:26 +0700

    change to 2 way ssl

--- environments/dev/config/params-local.php
+++ environments/dev/config/params-local.php
@@ -5,6 +5,6 @@ return [
     'TERMINAL_ID' => '10000061',
     'EPAY_OPERATOR_ID' => 'IBS',
     'EPAY_TERMINAL_ID' => '10000061',
-    'EPAY_URL' => 'wstest.oriongateway.com:22836',
+    'EPAY_URL' => 'wstest.oriongateway.com:22837',
     'EPAY_URL_PATH' => '/willani/services/oglws',
 ];
