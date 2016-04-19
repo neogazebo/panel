23dddd9d1734e935c4074568dfdefc8e5e2ae578
ilham <ilham@ebizu.com>
2016-04-19 19:31:21 +0700

    prepare all modules

--- views/layouts/menu.php
+++ views/layouts/menu.php
@@ -1,11 +1,11 @@
 <!-- sidebar menu: : style can be found in sidebar.less -->
 <ul class="sidebar-menu">
     <li class="header">MAIN NAVIGATION</li>
-    <li>
-        <a href="<?= Yii::$app->urlManager->createUrl('partner/index') ?>">
+    <li class="treeview">
+        <a href="#">
             <i class="fa fa-th-list"></i>
             <span>Partner</span>
-            <span class="label label-primary pull-right"></span>
+            <i class="fa fa-angle-left pull-right"></i>
         </a>
         <ul class="treeview-menu">
             <li>
