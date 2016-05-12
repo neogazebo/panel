957703427900e1d3ec3e12e9fe5c10a496bbaa11
Ganjar Setia <ganjar.setia@ebizu.com>
2016-05-12 19:47:58 +0700

    [ADMIN-562] Add generated module & modify index using Admin LTE style
    Signed-off-by: Ganjar Setia <ganjar.setia@ebizu.com>

---
+++ modules/merchant_signup/controllers/DefaultController.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace app\modules\merchant_signup\controllers;
+
+use Yii;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+use yii\filters\VerbFilter;
+use app\models\MerchantSignup;
+use app\models\MerchantSignupSearch;
+use yii\data\ActiveDataProvider;
+
+/**
+ * Default controller for the `MerchantSignup` module
+ */
+class DefaultController extends Controller
+{
+	private $_pageSize = 20;
+
+	public function behaviors()
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+        
+		$model = MerchantSignup::find();
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $model,
+            'pagination' => [
+                'pageSize' => $this->_pageSize,
+            ],
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    public function actionReview($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    protected function findModel($id)
+    {
+        if (($model = MerchantSignup::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
+}
