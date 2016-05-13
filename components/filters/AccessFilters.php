6d5dac28d067cd11b6d6a084222441a93a364f8e
ilham <ilham@ebizu.com>
2016-05-13 09:50:32 +0700

    not complete yet

--- components/filters/AccessFilters.php
+++ components/filters/AccessFilters.php
@@ -11,6 +11,8 @@ use yii\base\Module;
 use yii\web\ForbiddenHttpException;
 use app\models\User as UserAdmin;
 use app\components\helpers\GetRoutes;
+use app\models\AuthItem;
+use app\models\AuthItemChild;
 
 /**
 * 
@@ -36,12 +38,26 @@ class AccessFilters extends ActionFilter
 		if(!$rolename) {
 			throw new ForbiddenHttpException;
 		}
-		foreach ($rolename as $key) {
-			$permissionName = $auth->getPermissionsByRole($key->name);
-			// if($child->type == UserAdmin::TYPE_ROLE) {
-			// 	$chilPermision = $auth->getPermissionsByRole($key->name);
 
-			// }
+		foreach ($rolename as $key) {
+
+			$child = AuthItemChild::find()->where("parent = '$key->name'")->all();
+			foreach ($child as $key => $value) {
+				$getItemType = AuthItem::findOne($value->child);
+				if($getItemType->type == 1) {
+					$this->getChildRole($value->name);
+				}
+			}
+			exit;
+			$permissionName = $auth->getPermissionsByRole($key->name);
+			var_dump($permissionName);exit;
+			foreach ($permissionName as $key => $value) {
+				echo $value;exit;
+				if($value->type == UserAdmin::TYPE_ROLE) {
+					echo $value->name;exit;
+					$this->getChildRole($value->name);
+				}
+			}
 			if (array_key_exists($currentUrl, $permissionName)) {
 			    return true;
 			}else{
@@ -49,5 +65,40 @@ class AccessFilters extends ActionFilter
 			}
 		}
         return true;
+	}
+
+	public function getChildRole($role)
+	{
+		echo $role;exit;
+		if(!empty($role)) {
+			$auth = Yii::$app->authManager;
+			$userId = Yii::$app->user->identity->id;
+			$rolename = $auth->getRole($role);
+			$currentUrl = '/'.Yii::$app->requestedRoute;
+			if(!$rolename) {
+				throw new ForbiddenHttpException;
+			}
+			foreach ($rolename as $key) {
+				$permissionName = $auth->getPermissionsByRole($key->name);
+				if($key->type == UserAdmin::TYPE_ROLE) {
+					$chilPermision = $auth->getPermissionsByRole($key->name);
+					foreach ($chilPermision as $value) {	
+						$this->reUseGettingRole($value->name);
+					}
+				}
+				if (array_key_exists($currentUrl, $permissionName)) {
+				    return true;
+				}else{
+					throw new ForbiddenHttpException; 
+				}
+			}
+	        return true;
+		}
+		
+	}
+
+	public function reUseGettingRole($role)
+	{
+		return $this->getChildRole($role);
 	}
 }
