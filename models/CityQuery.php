b11aa7b26721f07591a6ce31a619da1ec8292b59
Ganjar Setia <ganjar.setia@ebizu.com>
2016-05-16 19:09:49 +0700

    [ADMIN-562] add model cityQuery from V2
    Signed-off-by: Ganjar Setia <ganjar.setia@ebizu.com>

---
+++ models/CityQuery.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\db\ActiveQuery;
+
+class CityQuery extends ActiveQuery
+{
+    public function getList()
+    {
+    	$this->andWhere('cit_status = :status', [':status' => 1]);
+        if(isset($_GET['search'])) {
+            $this->andWhere('cit_name LIKE :get', [':get' => '%' . $_GET['search'] . '%']);
+        }
+    	return $this;
+    }
+
+}
