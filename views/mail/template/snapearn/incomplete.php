b598e8489b8e720429c37f12a7e2f9f767476292
Elvis <elvis@ebizu.com>
2016-06-07 09:46:16 +0700

    added admin mail template for snapearn rejected

---
+++ views/mail/template/snapearn/incomplete.php
@@ -0,0 +1,31 @@
+<tbody>
+    <tr>
+    <td colspan="2"><h1 style="font-weight: 100; font-size: 30px; line-height: 60px; margin: 20px 0 0;">Hi&nbsp;[username]</h1></td>
+    <td width="100" style="display: table-cell; overflow: hidden;" rowspan="3">&nbsp;</td>
+    </tr>
+    <tr>
+        <td width="50">&nbsp;</td>
+        <td>
+            <p>We were unable to process your claim as the uploaded photo of your receipt
+                was incomplete Please snap another photo of your receipt and re-upload.
+            </p><br/>
+            <p>Ensure that these details are clearly displayed in the photo of your receipt:</p><br/>
+            <ol>
+                <li>Date &amp; Time of Receipt</li>
+                <li>Merchant Name</li>
+                <li>Transaction Amount</li>
+                <li>Receipt Number</li>
+            </ol>
+            <p>Below is a sample photo as a guideline.</p><br/>
+            <img src="[picture]" width="70%" /><br/>
+            <p>
+                Best Regards,<br/>
+                getmanis.com team
+            </p>
+        </td>
+    </tr>
+    <tr>
+        <td height="50">&nbsp;</td>
+        <td>&nbsp;</td>
+    </tr>
+</tbody>
---
+++ views/mail/template/snapearn/violates.php
@@ -0,0 +1,33 @@
+<tbody>
+    <tr>
+        <td colspan="2">
+            <h1 style="font-weight: 100; font-size: 30px; line-height: 60px; margin: 20px 0 0;">Hi&nbsp;[username]</h1>
+        </td>
+        <td width="100" style="display: table-cell; overflow: hidden;" rowspan="3">&nbsp;</td>
+    </tr>
+    <tr>
+        <td width="50">&nbsp;</td>
+        <td>
+        	<p>
+        		The Receipt that you uploaded for your Snap &amp; Earn from [business] @ <location> violates our Terms &amp; Conditions.
+        		Possible reasons for a receipt being invalid:
+        	</p><br/>
+        	<ol>
+        		<li>
+        			More than two (2) approved receipts from the same merchant have been uploaded on the same day
+        		</li>
+        	</ol><br/>
+        	<p>
+        		Please contact us at <a href="mailto:support@manis.com" target="_blank">support@manis.com</a> should you find that you received this message in error.
+        	</p><br/>
+        	<p>
+        		Best Regards,<br/>
+        		<a target="_blank" href="http://www.getmanis.com">getmanis.com</a> team
+        	</p>
+        </td>
+    </tr>
+    <tr>
+    	<td height="50">&nbsp;</td>
+    	<td>&nbsp;</td>
+    </tr>
+</tbody>
