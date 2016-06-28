b2ab3fabdf44331391149a1076d915ae6f7eca52
ilham <ilham@ebizu.com>
2016-06-28 09:48:02 +0700

    add db2

--- config/db2.php
+++ config/db2.php
@@ -2,9 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    // 'dsn' => 'mysql:host=46.137.207.152;dbname=manisv3_db',
-    'dsn' => 'mysql:host=dbx.ebizu.com;dbname=manisv3_db',
-    'username' => 'ebizu',
-    'password' => 'ebizu4ebizu',
+    'dsn' => 'mysql:host=manisdb.ebizu.com;dbname=manisv3_db',
+    'username' => 'manis',
+    'password' => 'EB!zu-43bizu*@',
     'charset' => 'utf8',
 ];
