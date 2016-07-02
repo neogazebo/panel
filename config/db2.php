76dc8ec6afa972bbd46b63996589553f0f5a0edd
ilham <ilham@ebizu.com>
2016-07-02 10:54:28 +0700

    [OVERTIME-419] Add Category id when adding new merchant and add existing merchant on snap and earn

--- config/db2.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=dbx.ebizu.com;dbname=manisv3_db',
-    'username' => 'ebizu',
-    'password' => 'ebizu4ebizu',
-    'charset' => 'utf8',
-];
