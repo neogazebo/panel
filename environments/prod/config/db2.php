6fee07a6b3c0800816042ef584dcb9bad18e7e44
ilham <ilham@ebizu.com>
2016-07-23 05:25:47 +0700

    [OVERTIME-437] create db slave master to prod config

--- environments/prod/config/db2.php
+++ environments/prod/config/db2.php
@@ -6,4 +6,16 @@ return [
     'username' => 'manis',
     'password' => 'EB!zu-43bizu*@',
     'charset' => 'utf8',
+    'slaveConfig' => [
+		'username' => 'admin',
+		'password' => 'EB!zu-43bizu*@',
+		'attributes' => [
+               PDO::ATTR_TIMEOUT => 10,
+           ],
+    'charset' => 'utf8',
+        ],
+    'slaves' => [
+                ['dsn' => 'mysql:host=manisdb2.ebizu.com;dbname=manisv3_db'],
+                ['dsn' => 'mysql:host=manisdb3.ebizu.com;dbname=manisv3_db']
+    ],
 ];
