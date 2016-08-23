5961e71670e75a61b1b885dc27edbbc64ed87e13
Elvis <elvis@ebizu.com>
2016-08-23 14:13:51 +0700

    [EAP-109] code review for edit hq

--- modules/system/processors/merchant_hq/MerchantHqChildrenSaveProcessor.php
+++ modules/system/processors/merchant_hq/MerchantHqChildrenSaveProcessor.php
@@ -1,128 +1,104 @@
 <?php
 
-    namespace app\modules\system\processors\merchant_hq;
+namespace app\modules\system\processors\merchant_hq;
 
-    use Yii;
+use Yii;
+use app\modules\system\processors\bases\BaseProcessor;
+use app\models\Company;
 
-    use app\modules\system\processors\bases\BaseProcessor;
-
-    use app\models\Company;
-
-    class MerchantHqChildrenSaveProcessor extends BaseProcessor
+class MerchantHqChildrenSaveProcessor extends BaseProcessor
+{
+    public function process()
     {
-        public function process()
-        {
-            $transaction = Yii::$app->db->beginTransaction();
+        $transaction = Yii::$app->db->beginTransaction();
 
-            try
-            {
-                $parent_id = Yii::$app->request->post('com_id');
-                $children = Yii::$app->request->post('children');
-                $op_confirmation = Yii::$app->request->post('op_confirmation');
-                $merchant_children = Company::find()->getChildMerchants($parent_id)->all();
-                $merchant_children_id = Company::find()->getChildMerchantsId($parent_id);
+        try {
+            $parent_id = Yii::$app->request->post('com_id');
+            $children = Yii::$app->request->post('children');
+            $op_confirmation = Yii::$app->request->post('op_confirmation');
+            $merchant_children = Company::find()->getChildMerchants($parent_id)->all();
+            $merchant_children_id = Company::find()->getChildMerchantsId($parent_id);
 
-                // if parent don't have any children yet
-                if(!$merchant_children)
-                {
-                    $children_names = Company::find()->getChildrenNames($children);
+            // if parent don't have any children yet
+            if(!$merchant_children) {
+                $children_names = Company::find()->getChildrenNames($children);
 
-                    // if children post data is empty
-                    if(!$children)
-                    {
-                        throw new \Exception("Please fill the merchants data", self::SYSTEM_ERROR_CODE);
+                // if children post data is empty
+                if(!$children)
+                    throw new \Exception("Please fill the merchants data", self::SYSTEM_ERROR_CODE);
+                
+                // if user have not confirm the operation yet
+                if(!$op_confirmation) {
+                    $message = Yii::$app->controller->renderPartial('partials/child_op', ['data' => $children_names, 'op' => 'add']);
+                    return $this->json_helper->jsonOutput(self::CHILDREN_OP_CONFIRMATION_CODE, 'error', $message);
+                }
+
+                Company::find()->saveMerchantChildren($parent_id, $children);
+                $transaction->commit();
+                return $this->json_helper->jsonOutput(0, 'success', null);
+            }
+
+            // if parent already have children
+            if($merchant_children) {
+                // if children post data is empty
+                if(!$children) {
+                    if(!$op_confirmation) {
+                        return $this->json_helper->jsonOutput(self::CHILDREN_OP_CONFIRMATION_CODE, 'error', 'You are about to empty the data');
                     }
-                    
-                    // if user have not confirm the operation yet
-                    if(!$op_confirmation)
-                    {
-                        $message = Yii::$app->controller->renderPartial('partials/child_op', ['data' => $children_names, 'op' => 'add']);
-                        return $this->json_helper->jsonOutput(self::CHILDREN_OP_CONFIRMATION_CODE, 'error', $message);
-                    }
 
-                    Company::find()->saveMerchantChildren($parent_id, $children);
+                    Company::find()->saveMerchantChildren(0, $merchant_children_id);
                     $transaction->commit();
                     return $this->json_helper->jsonOutput(0, 'success', null);
                 }
 
-                // if parent already have children
-                if($merchant_children)
-                {
-                    // if children post data is empty
-                    if(!$children)
-                    {
-                        if(!$op_confirmation)
-                        {
-                            return $this->json_helper->jsonOutput(self::CHILDREN_OP_CONFIRMATION_CODE, 'error', 'You are about to empty the data');
-                        }
+                // if children post data is not empty
+                // it's an add or remove op?
 
-                        Company::find()->saveMerchantChildren(0, $merchant_children_id);
-                        $transaction->commit();
-                        return $this->json_helper->jsonOutput(0, 'success', null);
+                // calculate the differences between posted data and db data
+                $changes = array_merge(
+                    array_diff($children, $merchant_children_id),
+                    array_diff($merchant_children_id, $children)
+                );
+                
+                if($changes) {
+                    // check also removed data
+                    $removed = array_diff($merchant_children_id, $children);
+                    $removed_children_names = Company::find()->getChildrenNames($removed);
+
+                    if($removed) {
+                        foreach($changes as $key => $change) {
+                            if(in_array($change, $removed))
+                                unset($changes[$key]);
+                        }
                     }
 
-                    // if children post data is not empty
-                    // it's an add or remove op?
+                    $children_names = Company::find()->getChildrenNames($changes);
 
-                    // calculate the differences between posted data and db data
-                    $changes = array_merge(array_diff($children, $merchant_children_id), array_diff($merchant_children_id, $children));
-                    
-                    if($changes)
-                    {
-                        // check also removed data
-                        $removed = array_diff($merchant_children_id, $children);
-                        $removed_children_names = Company::find()->getChildrenNames($removed);
+                    if(!$op_confirmation) {
+                        if(!$removed)
+                            $removed_children_names = null;
+                        
+                        $message = Yii::$app->controller->renderPartial('partials/child_op_combo', [
+                            'added_merchants' => $children_names, 
+                            'removed_merchants' => $removed_children_names, 
+                        ]);
 
-                        if($removed)
-                        {
-                            foreach($changes as $key => $change)
-                            {
-                                if(in_array($change, $removed))
-                                {
-                                    unset($changes[$key]);
-                                }
-                            }
-                        }
-
-                        $children_names = Company::find()->getChildrenNames($changes);
-
-                        if(!$op_confirmation)
-                        {
-                            if(!$removed)
-                            {
-                                $removed_children_names = null;
-                            }
-                            
-                            $message = Yii::$app->controller->renderPartial('partials/child_op_combo', [
-                                'added_merchants' => $children_names, 
-                                'removed_merchants' => $removed_children_names, 
-                            ]);
-
-                            return $this->json_helper->jsonOutput(self::CHILDREN_OP_CONFIRMATION_CODE, 'error', $message);
-                        }
-
-                        Company::find()->saveMerchantChildren($parent_id, $changes);
-
-                        if($removed)
-                        {
-                            Company::find()->saveMerchantChildren(0, $removed);
-                        }
-
-                        $transaction->commit();
-                        return $this->json_helper->jsonOutput(0, 'success', null);
+                        return $this->json_helper->jsonOutput(self::CHILDREN_OP_CONFIRMATION_CODE, 'error', $message);
                     }
-                    else
-                    {
-                        throw new \Exception("No changes were made, so no operation will be performed.", 1000);
-                    }
-                }
+
+                    Company::find()->saveMerchantChildren($parent_id, $changes);
+
+                    if($removed)
+                        Company::find()->saveMerchantChildren(0, $removed);
+
+                    $transaction->commit();
+                    return $this->json_helper->jsonOutput(0, 'success', null);
+                } else
+                    throw new \Exception('No changes were made, so no operation will be performed.', 1000);
             }
-            catch(\Exception $e)
-            {
-                $transaction->rollback();
-                return $this->json_helper->jsonOutput(1000, 'error', $e->getMessage());
-            }
-
-            
+        } catch(\Exception $e) {
+            $transaction->rollback();
+            return $this->json_helper->jsonOutput(1000, 'error', $e->getMessage());
         }
     }
+}
--- modules/system/processors/merchant_hq/MerchantHqChildrenSearchProcessor.php
+++ modules/system/processors/merchant_hq/MerchantHqChildrenSearchProcessor.php
@@ -1,29 +1,23 @@
 <?php
 
-    namespace app\modules\system\processors\merchant_hq;
+namespace app\modules\system\processors\merchant_hq;
 
-    use Yii;
+use Yii;
+use app\modules\system\processors\bases\BaseProcessor;
+use app\models\Company;
 
-    use app\modules\system\processors\bases\BaseProcessor;
-
-    use app\models\Company;
-
-    class MerchantHqChildrenSearchProcessor extends BaseProcessor
+class MerchantHqChildrenSearchProcessor extends BaseProcessor
+{
+    public function process()
     {
-        public function process()
-        {
-            try
-            {
-                $keyword = Yii::$app->request->post('keyword');
-                $hq_id = Yii::$app->request->post('hq_id');
+        try {
+            $keyword = Yii::$app->request->post('keyword');
+            $hq_id = Yii::$app->request->post('hq_id');
 
-                $data = Company::find()->searchMerchant($keyword, $hq_id);
-                $output = Yii::$app->controller->renderPartial('partials/search_result', ['data' => $data]);
-                return $this->json_helper->jsonOutput(0, 'success', null, $output);
-            }
-            catch(\Exception $e)
-            {
-                return $this->json_helper->jsonOutput(self::SYSTEM_ERROR_CODE, 'error', $e->getMessage());
-            }
-        }
+            $data = Company::find()->searchMerchant($keyword, $hq_id);
+            $output = Yii::$app->controller->renderPartial('partials/search_result', ['data' => $data]);
+            return $this->json_helper->jsonOutput(0, 'success', null, $output);
+        } catch(\Exception $e)
+            return $this->json_helper->jsonOutput(self::SYSTEM_ERROR_CODE, 'error', $e->getMessage());
     }
+}
