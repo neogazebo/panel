2c5a931aaf9c4333cc0a7d4d5f5690fd81a36826
ilham <ilham@ebizu.com>
2016-09-13 14:15:08 +0700

    [EBV-261] hotfix currency option and city

--- web/pages/SnapEarnManager.js
+++ web/pages/SnapEarnManager.js
@@ -16,43 +16,43 @@ $(document).ready(function() {
 
 	var $test = $(".search-merchant");
 
-	$(".search-merchant").select2({
-	  ajax: {
-	  	initSelection: function (element, callback) {
-	  		callback({ id: '', text: 'All' });
-		},
-	    url: search_mechant_url,
-	    dataType: 'json',
-	    delay: 250,
-	    data: function (params) {
-	      return {
-	        q: params.term, // search term
-	        page: params.page
-	      };
-	    },
-	    processResults: function (data, params) {
-	      // parse the results into the format expected by Select2
-	      // since we are using custom formatting functions we do not need to
-	      // alter the remote JSON data, except to indicate that infinite
-	      // scrolling can be used
-	      params.page = params.page || 1;
+	// $(".search-merchant").select2({
+	//   ajax: {
+	//   	initSelection: function (element, callback) {
+	//   		callback({ id: '', text: 'All' });
+	// 	},
+	//     url: search_mechant_url,
+	//     dataType: 'json',
+	//     delay: 250,
+	//     data: function (params) {
+	//       return {
+	//         q: params.term, // search term
+	//         page: params.page
+	//       };
+	//     },
+	//     processResults: function (data, params) {
+	//       // parse the results into the format expected by Select2
+	//       // since we are using custom formatting functions we do not need to
+	//       // alter the remote JSON data, except to indicate that infinite
+	//       // scrolling can be used
+	//       params.page = params.page || 1;
 
-	      return {
-	        results: data,
-	        /*
-	        pagination: {
-	          more: (params.page * 30) < data.length
-	        }
-	        */
-	      };
-	    },
-	    cache: true
-	  },
-	  escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
-	  minimumInputLength: 3,
-	  templateResult: formatData,
-	  templateSelection: formatRepoSelection
-	});
+	//       return {
+	//         results: data,
+	//         /*
+	//         pagination: {
+	//           more: (params.page * 30) < data.length
+	//         }
+	//         */
+	//       };
+	//     },
+	//     cache: true
+	//   },
+	//   escapeMarkup: function (markup) { return markup; }, // let our custom formatter work
+	//   minimumInputLength: 3,
+	//   templateResult: formatData,
+	//   templateSelection: formatRepoSelection
+	// });
 	
 	var timer;
     var x;
