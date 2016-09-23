7197cde0c3362387cffe13517e9bced0433bfe9b
Andre Hartiwan <andrehartiwan@Andres-MacBook-Air.local>
2016-09-23 18:21:07 +0700

    Fix multiple word role name bug

--- modules/rbac/views/index/user.php
+++ modules/rbac/views/index/user.php
@@ -23,8 +23,8 @@ $dataProvider->sort->attributes['user.username'] = [
                 <div class="box-header with-border">
                     <?= Html::a('<i class="fa fa-chevron-left"></i> Back', ['cancel'], ['class' => 'btn btn-flat btn-success btn-sm']) ?>
                     <div class="pull-right">
-                        <?= Html::button('<i class="fa fa-plus-square"></i> Assign User', ['type' => 'button','value' => Url::to(['assign?role=' .$name]), 'class' => 'modalButton btn btn-flat btn-warning btn-sm']); ?> 
-                        <?= Html::a('<i class="fa fa-plus-square"></i> Multi Assign', ['multi-assign?role='.$name], ['class' => 'btn btn-flat btn-success btn-sm']) ?>
+                        <?= Html::button('<i class="fa fa-plus-square"></i> Assign User', ['type' => 'button','value' => Url::to(['assign?role=' . urlencode($name)]), 'class' => 'modalButton btn btn-flat btn-warning btn-sm']); ?> 
+                        <?= Html::a('<i class="fa fa-plus-square"></i> Multi Assign', ['multi-assign?role='.urlencode($name)], ['class' => 'btn btn-flat btn-success btn-sm']) ?>
                     </div>
                 </div>
                 <div class="box-body">
