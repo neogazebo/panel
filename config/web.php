957143c8fac93eaff12584d1ac201c0ff2c3dd5c
ilham <ilham@ebizu.com>
2016-10-04 14:44:58 +0700

    add config swiftmailer

--- config/web.php
+++ config/web.php
@@ -99,10 +99,17 @@ $config = [
         ],
         'mailer' => [
             'class' => 'yii\swiftmailer\Mailer',
-            // send all mails to a file by default. You have to set
-            // 'useFileTransport' to false and configure a transport
-            // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'transport' => [
+                'class' => 'Swift_SmtpTransport',
+                'constructArgs' => ['smtp.getmanis.com', 25]
+                'plugins' => [
+                    [
+                        'class' => 'Swift_Plugins_ThrottlerPlugin',
+                        'constructArgs' => [20],
+                    ],
+                ],
+            ],
+            // 'useFileTransport' => true,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
